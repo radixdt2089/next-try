96ad1dacc0a1f440103bda44158754f162cb07c7
ankur.chovatiya <ankur.chovatiya@radixweb.com>
2022-02-09 16:17:14 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fd23b786c5c413c4743939912a0ed054'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e9251c3-a4f0-4db0-8cb8-f130f7d60707'
+};
